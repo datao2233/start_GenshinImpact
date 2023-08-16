89802d2c2349aeae80dc2ce20f761e1fe85ae073
datao2233 <122881769+datao2233@users.noreply.github.com>
2023-08-16 13:56:04 +0800

    Add files via upload

---
+++ 一键环境修复.bat
@@ -0,0 +1,5 @@
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install numpy
+pip install pypiwin32
+pip install pygame
