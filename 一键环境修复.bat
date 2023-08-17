280b7f4a1fcdbc03c17c0b7aab9f2654fb5beb19
datao2233 <122881769+datao2233@users.noreply.github.com>
2023-08-17 12:55:28 +0800

    Update 一键环境修复.bat

--- 一键环境修复.bat
+++ 一键环境修复.bat
@@ -1,5 +1,11 @@
 python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install pillow
+
 pip install numpy
+
 pip install pypiwin32
+
 pip install pygame
