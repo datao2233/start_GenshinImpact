6c99cc4d3dfcdbb773d030a5bab914a0169bd3d8
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-02 14:30:14 +0800

    不直接设置镜像源以防影响其他的pip指令并关闭回显

--- 一键环境修复.bat
+++ 一键环境修复.bat
@@ -1,11 +1,14 @@
+@echo off
+
 python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+REM 更新pip(镜像源)
 
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-pip install pillow
+pip install numpy -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-pip install numpy
+pip install pypiwin32 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-pip install pypiwin32
+pip install pygame -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-pip install pygame
+REM 不直接设置镜像源以防影响其他的pip指令
