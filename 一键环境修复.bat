d6eb3ed0cdf8f57695000cdeaf2fc90fab688fca
datao2233 <122881769+datao2233@users.noreply.github.com>
2024-05-02 16:32:00 +0800

    Update 一键环境修复.bat

--- 一键环境修复.bat
+++ 一键环境修复.bat
@@ -1,7 +1,4 @@
-@echo off
-
 python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
-REM 更新pip(镜像源)
 
 pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 
@@ -12,6 +9,3 @@ pip install pypiwin32 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install pygame -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 pip install psutil -i https://pypi.tuna.tsinghua.edu.cn/simple
-REM 用于检测是否已经启动
-
-REM 不直接设置镜像源以防影响其他的pip指令
